49c91629d58159ac6da8da979df3fdd169f6cbcb
ZiyunZhao <zhaoziyun@berkeley.edu>
2020-06-27 23:45:51 -0700

    Updates

---
+++ precache-manifest.5735ee8ac23466ebd51fe567d90ef417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991a947d76990b826ed8d4b772991b58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0edca8be0a0e2a1253f0",
+    "url": "/static/css/main.e1636c0a.chunk.css"
+  },
+  {
+    "revision": "4e57f0cb3af3e12953fc",
+    "url": "/static/js/2.bdd2fc31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bdd2fc31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0edca8be0a0e2a1253f0",
+    "url": "/static/js/main.04373e78.chunk.js"
+  },
+  {
+    "revision": "aab2099d422c4f313100",
+    "url": "/static/js/runtime-main.54ec0ee2.js"
+  }
+]);
